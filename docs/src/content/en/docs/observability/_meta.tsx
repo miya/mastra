0fd05fe05ff9d2e6d23afc3892d5221e9548b0ea
Eric Pinzur <epinzur@gmail.com>
2025-08-22 19:51:19 +0200

    lint

--- docs/src/content/en/docs/observability/_meta.tsx
+++ docs/src/content/en/docs/observability/_meta.tsx
@@ -3,7 +3,7 @@ const meta = {
   logging: "Logging",
   tracing: "Tracing",
   "nextjs-tracing": { title: "Next.js Tracing", display: "hidden" },
-  "ai-tracing":<Tag text="experimental">AI Tracing</Tag>,
+  "ai-tracing": <Tag text="experimental">AI Tracing</Tag>,
 };
 
 export default meta;
